d83d88c99b2e3c07d627ae68fe9eca3cece616b6
Gilberto Placidon <gilbertoplacidon@gmail.com>
2021-12-27 00:25:06 -0800

    Updates

---
+++ precache-manifest.b1c5038424c72adf3de13d656467498b.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e5077123f9a83d7ba1201bb6f7a16d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f1f0010c55cce0ae545",
+    "url": "/static/css/2.1be12fd0.chunk.css"
+  },
+  {
+    "revision": "2a2f898013de7c78fc78",
+    "url": "/static/css/main.d749d290.chunk.css"
+  },
+  {
+    "revision": "7f1f0010c55cce0ae545",
+    "url": "/static/js/2.b035b8b3.chunk.js"
+  },
+  {
+    "revision": "752c3c2f181e565a944434916a153d16",
+    "url": "/static/js/2.b035b8b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2f898013de7c78fc78",
+    "url": "/static/js/main.128ee912.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "bcf96e94c10b30d98993e0744782a42c",
+    "url": "/static/media/Web_Design_GinaCulb.bcf96e94.png"
+  },
+  {
+    "revision": "ccf5070cd141099374c5153c170e50ad",
+    "url": "/static/media/Web_Design_MDL.ccf5070c.png"
+  },
+  {
+    "revision": "9cf8ff4743e8891ed6de60af194694e5",
+    "url": "/static/media/Web_Design_TempCulb.9cf8ff47.png"
+  },
+  {
+    "revision": "3972949aec9c96e42dc3b54a3974596b",
+    "url": "/static/media/airplane.3972949a.png"
+  },
+  {
+    "revision": "f9a2228d683b138967180ff11697a9db",
+    "url": "/static/media/all.f9a2228d.svg"
+  },
+  {
+    "revision": "0be93193211413fced826185db7769bd",
+    "url": "/static/media/culb_1.0be93193.png"
+  },
+  {
+    "revision": "deb70cbdf774ba6ed06e241a8d4e47dd",
+    "url": "/static/media/culb_2.deb70cbd.png"
+  },
+  {
+    "revision": "2b09ae730d3c51686a933fba11c095f1",
+    "url": "/static/media/culb_3.2b09ae73.png"
+  },
+  {
+    "revision": "0aec45567767d178a07442566c125721",
+    "url": "/static/media/culb_4.0aec4556.png"
+  },
+  {
+    "revision": "65ecac8e47e436355ae5120741cc6d52",
+    "url": "/static/media/culb_5.65ecac8e.png"
+  },
+  {
+    "revision": "8a7f5a93a376e6cc09084cdeb9cde3a7",
+    "url": "/static/media/culb_6.8a7f5a93.png"
+  },
+  {
+    "revision": "292ce1f2d4bbf649c547a2fa5d9a60f5",
+    "url": "/static/media/culb_7.292ce1f2.png"
+  },
+  {
+    "revision": "b2c71a4b555ae301816b23954fec1a33",
+    "url": "/static/media/earth_gif.b2c71a4b.gif"
+  },
+  {
+    "revision": "725615b09ded3e642c5788451ca53561",
+    "url": "/static/media/gina_1.725615b0.png"
+  },
+  {
+    "revision": "44115e780a30c385c9efa052ca4f80e2",
+    "url": "/static/media/gina_2.44115e78.png"
+  },
+  {
+    "revision": "843f697e5c598e7c81f99937854b69be",
+    "url": "/static/media/gina_3.843f697e.png"
+  },
+  {
+    "revision": "26c660399ff18d2974543fc084a24d99",
+    "url": "/static/media/gina_4.26c66039.png"
+  },
+  {
+    "revision": "5f6bbe01561e94abc3b44c8e72973aa7",
+    "url": "/static/media/gina_5.5f6bbe01.png"
+  },
+  {
+    "revision": "126e4dbc0930d00ec1160a5677a59c1f",
+    "url": "/static/media/gina_6.126e4dbc.png"
+  },
+  {
+    "revision": "62278d77cc29fe4c21d66b0d185db14c",
+    "url": "/static/media/gina_7.62278d77.png"
+  },
+  {
+    "revision": "17d4a1afb940ecd52e3c67ea526db02b",
+    "url": "/static/media/mdl_1.17d4a1af.png"
+  },
+  {
+    "revision": "5b4a8d29178e33dbf47be07246c44b34",
+    "url": "/static/media/mdl_2.5b4a8d29.png"
+  },
+  {
+    "revision": "fb0a35e187761520e69728cd1c4c5fa9",
+    "url": "/static/media/tm_flower.fb0a35e1.gif"
+  },
+  {
+    "revision": "f9955c52700f80c94384b72bde586369",
+    "url": "/static/media/tm_flower_frame.f9955c52.png"
+  },
+  {
+    "revision": "5925a2c46b66353a0f01c17f9437c722",
+    "url": "/static/media/transistor_gif.5925a2c4.gif"
+  },
+  {
+    "revision": "4432bce4b876a09ba27ddd57c232a02a",
+    "url": "/static/media/transistor_sword.4432bce4.png"
+  }
+]);
