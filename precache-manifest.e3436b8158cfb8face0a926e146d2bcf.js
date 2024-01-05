598abeeb214f69d7dd2ec414636065003ea2f229
Gilberto Placidon <gilbertoplacidon@gmail.com>
2024-01-04 17:01:50 -0800

    Testing build upload

---
+++ precache-manifest.e3436b8158cfb8face0a926e146d2bcf.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a91fc62f9ac0ed9b549ff5c8a59b9b",
+    "url": "/Gilberto_Placidon_Portfolio/index.html"
+  },
+  {
+    "revision": "214726ed78566b555eb8",
+    "url": "/Gilberto_Placidon_Portfolio/static/css/2.e6d2f6ae.chunk.css"
+  },
+  {
+    "revision": "5202d58b0207f8037394",
+    "url": "/Gilberto_Placidon_Portfolio/static/css/main.ef42baf8.chunk.css"
+  },
+  {
+    "revision": "214726ed78566b555eb8",
+    "url": "/Gilberto_Placidon_Portfolio/static/js/2.ec850225.chunk.js"
+  },
+  {
+    "revision": "96024c60d825b06ad12faa8aa42e8308",
+    "url": "/Gilberto_Placidon_Portfolio/static/js/2.ec850225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5202d58b0207f8037394",
+    "url": "/Gilberto_Placidon_Portfolio/static/js/main.18216758.chunk.js"
+  },
+  {
+    "revision": "e493fffed7b1623b6907",
+    "url": "/Gilberto_Placidon_Portfolio/static/js/runtime-main.7e5bcbcc.js"
+  },
+  {
+    "revision": "bcf96e94c10b30d98993e0744782a42c",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/Web_Design_GinaCulb.bcf96e94.png"
+  },
+  {
+    "revision": "ccf5070cd141099374c5153c170e50ad",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/Web_Design_MDL.ccf5070c.png"
+  },
+  {
+    "revision": "9cf8ff4743e8891ed6de60af194694e5",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/Web_Design_TempCulb.9cf8ff47.png"
+  },
+  {
+    "revision": "3972949aec9c96e42dc3b54a3974596b",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/airplane.3972949a.png"
+  },
+  {
+    "revision": "0be93193211413fced826185db7769bd",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/culb_1.0be93193.png"
+  },
+  {
+    "revision": "deb70cbdf774ba6ed06e241a8d4e47dd",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/culb_2.deb70cbd.png"
+  },
+  {
+    "revision": "2b09ae730d3c51686a933fba11c095f1",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/culb_3.2b09ae73.png"
+  },
+  {
+    "revision": "0aec45567767d178a07442566c125721",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/culb_4.0aec4556.png"
+  },
+  {
+    "revision": "65ecac8e47e436355ae5120741cc6d52",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/culb_5.65ecac8e.png"
+  },
+  {
+    "revision": "8a7f5a93a376e6cc09084cdeb9cde3a7",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/culb_6.8a7f5a93.png"
+  },
+  {
+    "revision": "292ce1f2d4bbf649c547a2fa5d9a60f5",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/culb_7.292ce1f2.png"
+  },
+  {
+    "revision": "b2c71a4b555ae301816b23954fec1a33",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/earth_gif.b2c71a4b.gif"
+  },
+  {
+    "revision": "725615b09ded3e642c5788451ca53561",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/gina_1.725615b0.png"
+  },
+  {
+    "revision": "44115e780a30c385c9efa052ca4f80e2",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/gina_2.44115e78.png"
+  },
+  {
+    "revision": "843f697e5c598e7c81f99937854b69be",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/gina_3.843f697e.png"
+  },
+  {
+    "revision": "26c660399ff18d2974543fc084a24d99",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/gina_4.26c66039.png"
+  },
+  {
+    "revision": "5f6bbe01561e94abc3b44c8e72973aa7",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/gina_5.5f6bbe01.png"
+  },
+  {
+    "revision": "126e4dbc0930d00ec1160a5677a59c1f",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/gina_6.126e4dbc.png"
+  },
+  {
+    "revision": "62278d77cc29fe4c21d66b0d185db14c",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/gina_7.62278d77.png"
+  },
+  {
+    "revision": "fd5c7a4674964bb863437af5e1d04b4c",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/gsap-logo1.5x.fd5c7a46.png"
+  },
+  {
+    "revision": "17d4a1afb940ecd52e3c67ea526db02b",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/mdl_1.17d4a1af.png"
+  },
+  {
+    "revision": "5b4a8d29178e33dbf47be07246c44b34",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/mdl_2.5b4a8d29.png"
+  },
+  {
+    "revision": "fb0a35e187761520e69728cd1c4c5fa9",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/tm_flower.fb0a35e1.gif"
+  },
+  {
+    "revision": "f9955c52700f80c94384b72bde586369",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/tm_flower_frame.f9955c52.png"
+  },
+  {
+    "revision": "5925a2c46b66353a0f01c17f9437c722",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/transistor_gif.5925a2c4.gif"
+  },
+  {
+    "revision": "4432bce4b876a09ba27ddd57c232a02a",
+    "url": "/Gilberto_Placidon_Portfolio/static/media/transistor_sword.4432bce4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1c5038424c72adf3de13d656467498b.js"
+  "/Gilberto_Placidon_Portfolio/precache-manifest.e3436b8158cfb8face0a926e146d2bcf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Gilberto_Placidon_Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
